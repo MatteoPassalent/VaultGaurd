3640d0450543227dd7d35eaf336b1b94a78f33c8
MatteoPassalent <pass2102@gmail.com>
2023-03-31 15:32:39 -0400

    Final Test2

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app = create_app()
 # test
 # test 2
 # test 3
+# final test
 # without this line, it will run when imported, debug=true automatically reruns when edits are made, __name__ is special variable
 if __name__ == '__main__':
     app.run(debug=True)
