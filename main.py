602b146250ac3c8749ad135c2e37c067dd96a702
MatteoPassalent <pass2102@gmail.com>
2023-03-31 14:45:17 -0400

    test2

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from web_app import create_app
 app = create_app()
 
 # test
+# test 2
 # without this line, it will run when imported, debug=true automatically reruns when edits are made, __name__ is special variable
 if __name__ == '__main__':
     app.run(debug=True)
