a5766528ad699e73f0205ea8ec3b8fe203084896
MatteoPassalent <pass2102@gmail.com>
2023-04-06 17:18:04 -0400

    Commenting 1

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+
+# COME BACK LATER AND DO PROPER FUNCTION DESCRIPTIONS FOR REVIEW
+# SQLALchemy is an Object Relational Mapper (ORM), allows easier interaction with database using a object oriented approach instead of raw SQL queiries
 from flask_sqlalchemy import SQLAlchemy
-# from operating system import path
+
+# from operating system import path module which contains functions related to file paths
 from os import path
 from flask_login import LoginManager
 
@@ -8,54 +12,50 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
-# create_app function, app = flask(__name__) initliazes app, secret_key is a secret key. Why does this function run?
-
-
+# create_app function, app = flask(__name__) initliazes app.
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'matteo'
+    app.config["SECRET_KEY"] = "matteo"
     # database is located at that location
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    # app will be used for db database
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    # Initializes and connects database with this flask app
     db.init_app(app)
 
     # dot is reletive import, it means its from the same website folder
+    # imports view, auth blueprints and imports User class
     from .views import views
     from .auth import auth
     from .models import User
 
-    # prefix is what is placed before url in route brackets, dont want anything here
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
-
-    from .models import User
-
-    create_database(app)
+    # Registers previously imported blueprints with the main flask app
+    app.register_blueprint(views)
+    app.register_blueprint(auth)
 
     # initializes login manager
-
     # Create a LoginManger instance
     login_manager = LoginManager()
-
-    # Define the scope
-    login_manager.login_view = 'auth.login'
-
+    # Sets the login_view attribute to the login function in the auth blueprint. @login_required will redirect to auth.login function if user is not logged in
+    login_manager.login_view = "auth.login"
     # Msg defaults to true, force none
     login_manager.login_message = None
-
-    # Connect application
+    # Connects flask application
     login_manager.init_app(app)
 
-    # tells flask how to load a user, query default looks for primary key. Returns id
+    # tells flask how to load a user, query looks for primary key. Returns id or none if user doesnt exist. Is not called directly but instead through internal loginmanager functions
+    # The decorator registers the function as the "user loader callback function". Which loads a user object from the database
     @login_manager.user_loader
+    # id is the unique user id defined in the User class in the models module
     def load_user(id):
+        # queries the database for a user object with a matching id
         return User.query.get(int(id))
+
+    # Calls create_database function below
+    create_database(app)
 
     return app
 
+
 # creates database only if it doesnt already exist
-
-
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
+    if not path.exists("VaultGuard_Password_Manager/" + DB_NAME):
         db.create_all(app=app)
--- web_app/views.py
+++ web_app/views.py
@@ -4,113 +4,61 @@ from . import db
 from .models import Account
 import json
 
-# Blueprint holds multiple routes inside
-views = Blueprint('views', __name__)
+# creates blueprint (mini flask app). Blueprint holds multiple routes inside
+views = Blueprint("views", __name__)
 
-
-@views.route('/', methods=['GET'])
+# function below will run when the url with '/' prefix is accessed. ('views' blueprint replaces 'app')
+# Handles http get request
+@views.route("/", methods=["GET"])
+# flask_login decorator that redirects user to auth.login function if not logged in. (set in init "login_manager.login_view = 'auth.login'")
 @login_required
 def home():
     return render_template("home.html", user=current_user)
 
-# @ is decorator, function below will run whenever you go to '/' website
 
-
-@views.route('/', methods=['POST'])
+@views.route("/", methods=["POST"])
 @login_required
 def add_account():
     # current user returns authenticated if logged in. Can use: user.is_aunthenticated in base.html
-    if request.method == 'POST':
-        AccountTitle = request.form.get('accountTitle')
-        UserName = request.form.get('userName')
-        Email = request.form.get('email')
-        Password = request.form.get('password')
-        Additional = request.form.get('additional')
+    if request.method == "POST":
+        AccountTitle = request.form.get("accountTitle")
+        UserName = request.form.get("userName")
+        Email = request.form.get("email")
+        Password = request.form.get("password")
+        Additional = request.form.get("additional")
 
         if len(AccountTitle) < 1:
-            flash('Account Title required', category='error')
+            flash("Account Title required", category="error")
         elif len(AccountTitle) > 20:
-            flash('Account Title Too Long', category='error')
+            flash("Account Title Too Long", category="error")
         elif len(UserName) > 21:
-            flash('Username is too long', category='error')
+            flash("Username is too long", category="error")
         elif len(Password) > 50:
-            flash('Password is too long', category='error')
+            flash("Password is too long", category="error")
         elif len(Email) > 35:
-            flash('Email is too long', category='error')
+            flash("Email is too long", category="error")
         elif len(Additional) > 130:
-            flash('Additional Info is too long', category='error')
+            flash("Additional Info is too long", category="error")
         else:
-            new_Account = Account(AccountTitle=AccountTitle, UserName=UserName, Email=Email,
-                                  Password=Password, Additional=Additional, user_id=current_user.id)
+            new_Account = Account(
+                AccountTitle=AccountTitle,
+                UserName=UserName,
+                Email=Email,
+                Password=Password,
+                Additional=Additional,
+                user_id=current_user.id,
+            )
             db.session.add(new_Account)
             db.session.commit()
-            flash('Account added!', category='success')
+            flash("Account added!", category="success")
 
-    return redirect(url_for('views.home'))
+    return redirect(url_for("views.home"))
 
 
-@views.route('/delete-account', methods=['POST'])
+@views.route("/delete-account", methods=["POST"])
 def delete_account():
     account = json.loads(request.data)
-    account_id = account['accountId']
-    account = Account.query.get(account_id)
-    if account:
-        if account.user_id == current_user.id:
-            db.session.delete(account)
-            db.session.commit()
-
-    return jsonify({})
-
-
-# Blueprint holds multiple routes inside
-views = Blueprint('views', __name__)
-
-
-@views.route('/', methods=['GET'])
-@login_required
-def home():
-    return render_template("home.html", user=current_user)
-
-# @ is decorator, function below will run whenever you go to '/' website
-
-
-@views.route('/', methods=['POST'])
-@login_required
-def add_account():
-    # current user returns authenticated if logged in. Can use: user.is_aunthenticated in base.html
-    if request.method == 'POST':
-        AccountTitle = request.form.get('accountTitle')
-        UserName = request.form.get('userName')
-        Email = request.form.get('email')
-        Password = request.form.get('password')
-        Additional = request.form.get('additional')
-
-        if len(AccountTitle) < 1:
-            flash('Account Title required', category='error')
-        elif len(AccountTitle) > 20:
-            flash('Account Title Too Long', category='error')
-        elif len(UserName) > 21:
-            flash('Username is too long', category='error')
-        elif len(Password) > 50:
-            flash('Password is too long', category='error')
-        elif len(Email) > 35:
-            flash('Email is too long', category='error')
-        elif len(Additional) > 130:
-            flash('Additional Info is too long', category='error')
-        else:
-            new_Account = Account(AccountTitle=AccountTitle, UserName=UserName, Email=Email,
-                                  Password=Password, Additional=Additional, user_id=current_user.id)
-            db.session.add(new_Account)
-            db.session.commit()
-            flash('Account added!', category='success')
-
-    return redirect(url_for('views.home'))
-
-
-@views.route('/delete-account', methods=['POST'])
-def delete_account():
-    account = json.loads(request.data)
-    account_id = account['accountId']
+    account_id = account["accountId"]
     account = Account.query.get(account_id)
     if account:
         if account.user_id == current_user.id:
